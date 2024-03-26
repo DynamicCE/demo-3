5c968649fe91fa3e11990b6206a7c561477d841e
Adınız <erkanmurat0632@gmail.com>
2024-03-26 13:56:19 +0300

    ı

--- src/main/java/northwindProject/demo/business/abstracts/ProductService.java
+++ src/main/java/northwindProject/demo/business/abstracts/ProductService.java
@@ -1,9 +1,10 @@
 package northwindProject.demo.business.abstracts;
 
 import northwindProject.demo.entities.concretes.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public
 interface ProductService {
     List<Product> getAll();
