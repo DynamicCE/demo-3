dfd6fd84a32678295d75fa76161e12c5d08c1049
Adınız <erkanmurat0632@gmail.com>
2024-03-26 14:20:30 +0300

    new

--- src/main/java/northwindProject/demo/business/concretes/ProductManager.java
+++ src/main/java/northwindProject/demo/business/concretes/ProductManager.java
@@ -1,15 +1,27 @@
 package northwindProject.demo.business.concretes;
 
+import java.util.List;
+
+import northwindProject.demo.dataAccess.abstracts.ProductDao;
+import org.springframework.stereotype.Service;
+
 import northwindProject.demo.business.abstracts.ProductService;
 import northwindProject.demo.entities.concretes.Product;
 
-import java.util.List;
-
+@Service
 public
 class ProductManager implements ProductService {
+    private
+    ProductDao productDao;
+
+    public
+    ProductManager ( ProductDao productDao ) {
+        this.productDao = productDao;
+    }
+
     @Override
     public
     List<Product> getAll () {
-        return null;
+        return productDao.findAll ( );
     }
 }
